21a748877d70634a47af10cf47c9e79e4f05ae34
Danio Fiore <danio.fiore@sysco.biz>
2024-06-13 09:10:13 +0200

    added to do

--- src/App.vue
+++ src/App.vue
@@ -4,6 +4,7 @@
     <router-link to="/about">About</router-link>
   </nav>
   <router-view/> -->
+  <!-- TODO: check why the HomeView component is not visible -->
   <div>
     <HomeView />
   </div>
